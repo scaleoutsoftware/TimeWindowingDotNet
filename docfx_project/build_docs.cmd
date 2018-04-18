dd1114b68268ade1c369fa95c6a098fd45c0bbb8
markwaterman <markwaterman@users.noreply.github.com>
2018-04-18 15:10:32 -0700

    Add build_docs.cmd with some comments to describe what's involved.

---
+++ docfx_project/build_docs.cmd
@@ -0,0 +1,6 @@
+:: Requires DocFX tool: https://github.com/dotnet/docfx
+:: Documentation build output goes to ../docs/
+:: so it can be served up by github pages at
+:: https://scaleoutsoftware.github.io/TimeWindowingDotNet/
+
+docfx.exe
